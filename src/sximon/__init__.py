f4ac5b3117212c30b934129f01e31f814feab60c
TakesxiSximada <takesxi.sximada@gmail.com>
2015-04-24 23:26:20 +0900

    WIP

--- src/sximon/__init__.py
+++ src/sximon/__init__.py
@@ -1,1 +1,18 @@
 # -*- coding: utf-8 -*-
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+
+
+def hello_world(request):
+    return Response('Hello %(name)s!' % request.matchdict)
+
+
+def main(global_config, **local_config):
+    """This function returns a Pyramid application.
+    """
+    settings = dict(global_config)
+    settings.update(local_config)
+
+    config = Configurator(settings=settings)
+    return config.make_wsgi_app()
